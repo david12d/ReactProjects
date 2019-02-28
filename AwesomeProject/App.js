94985dc6722ab9c5a6c0d7e3d219a05aad0a0dd4
David-Desktop <david12ding@gmail.com>
2019-02-28 12:34:21 +1100

    style for awesomeproject

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, AppRegistry, Text } from 'react-native';
+import { StyleSheet, View, AppRegistry, Text } from 'react-native';
 
 
 /* Lesson 1: Hello World
@@ -109,4 +109,29 @@ import { View, AppRegistry, Text } from 'react-native';
 
 /* Lesson 5: Style
 */
+  const styles = StyleSheet.create({
+    bigBlue: {
+      color: 'blue',
+      fontWeight: 'bold',
+      fontSize: 30,
+    },
+    red: {
+      color: 'red',
+    },
+  });
 
+  export default class LotsOfStyles extends Component {
+    render() {
+      return (
+        <View>
+          <Text style={styles.red}>just red</Text>
+          <Text style={styles.bigBlue}>just bigBlue</Text>
+          <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+          <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+        </View>
+      );
+    }
+  }
+
+  // skip this line if using Create React Native App
+  AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
